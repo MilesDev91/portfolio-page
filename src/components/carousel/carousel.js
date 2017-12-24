32d0bdea53c9c798a2d691ab723f4755ff593fec
Miles Coffin <micoffin91@gmail.com>
2017-12-24 01:41:12 -0500

    Finished button animations, switched to img elements (instead of background images), and set up animation/timeout handlers. Added timeout utility file.

--- src/components/carousel/carousel.js
+++ src/components/carousel/carousel.js
@@ -1,39 +1,92 @@
 import { h, Component } from 'preact';
 import classes from './Carousel.css';
 import { Icon } from 'react-fa';
+import CSSTransition from 'react-transition-group/CSSTransition';
+import Timeout from '../../utility/timeout';
 
-export default class ComponentName extends Component {
+const animationTiming = 500;
+
+class Carousel extends Component {
 
     state = {
-        position: 1
+        position: 1,
+        leftButtonClass: [],
+        rightButtonClass: [],
+        imageClass: []
     }
 
-    positionChangedHandler = () => {
+    btnAnimTrigger = false;
 
+    positionChangedHandler = (direction) => {
+        if (direction === "left") {
+            this.buttonAnimationHandler("left");
+        } else {
+            this.setState({ position: this.state.position + 1 });
+            this.buttonAnimationHandler("right");
+        }
+
+    }
+
+    buttonAnimationHandler = (direction) => {
+        let btnClass = [];
+        if (!this.btnAnimTrigger) {
+            console.log("in button handler");
+            this.btnAnimTrigger = true;
+            if (direction === "left") {
+                btnClass.push(classes.buttonAnimationLeft);
+                this.setState({ leftButtonClass: btnClass });
+                this.props.clearTimeouts();
+                this.timeoutHandler({ leftButtonClass: [] });
+            } else {
+                btnClass.push(classes.buttonAnimationRight);
+                this.setState({ rightButtonClass: btnClass });
+                this.props.clearTimeouts();
+                this.timeoutHandler({ rightButtonClass: [] });
+            }
+        }
+    }
+
+    imageAnimationHandler = (direction) => {
+        let imgClass = [];
+        imgClass.push(classes.carousel1to2);
+        this.setState({ imageClass: imgClass });
+    }
+
+    timeoutHandler (stateChange) {
+        this.props.setTimeout(() => {
+            this.setState(stateChange);
+            console.log(this.state.leftButtonClass);
+            this.btnAnimTrigger = false;
+        }, animationTiming);
     }
 
     render () {
-        let carouselImage = [classes.carousel];
-        switch (this.state.position) {
-            case 1:
-                carouselImage.push(classes.carousel1);
-                break;
-            case 2:
-                carouselImage.push(classes.carousel2);
-                break;
-            default:
-                return null;
-        }
+        //let carouselImage = [classes.carousel];
+        // switch (this.state.position) {
+        //     case 1:
+        //         carouselImage.push(classes.carousel1);
+        //         break;
+        //     case 2:
+        //         carouselImage.push(classes.carousel1to2);
+        //         break;
+        //     default:
+        //         return nusll;
+        //}
         return (
-            <div class={carouselImage.join(' ')}>
+            <div class={classes.carousel}>
+                <img src="../../assets/image/Initial.jpeg" />
+                <img src="../../assets/image/Second.jpeg" />
+                <img src="../../assets/image/Third.jpeg" />
                 <div class={classes.overlay}>
                     <div class={classes.middle}>
-                        <button><Icon name="angle-left" size="3x" /></button>
+                        <button class={this.state.leftButtonClass.join(' ')} onClick={() => this.positionChangedHandler("left")}><Icon name="angle-left" size="3x" /></button>
                         <p>Hi I am a carousel</p>
-                        <button><Icon name="angle-right" size="3x" /></button>
+                        <button class={this.state.rightButtonClass.join(' ')} onClick={() => this.positionChangedHandler("right")}><Icon name="angle-right" size="3x" /></button>
                     </div>
                 </div>
-            </div>
+            </div >
         );
     }
 }
+
+export default Timeout(Carousel);
---
+++ src/utility/timeout.js
@@ -0,0 +1,40 @@
+/*Needed a solution to clear timeouts  
+/ Solution provided by jmgem on stackoverflow
+/ Thanks!!!
+*/
+
+import { h, Component } from 'preact';
+
+const Timeout = Composition => class _Timeout extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    componentWillMount () {
+        this.timeouts = [];
+    }
+
+    setTimeout () {
+        this.timeouts.push(setTimeout.apply(null, arguments));
+    }
+
+    clearTimeouts () {
+        this.timeouts.forEach(clearTimeout);
+    }
+
+    componentWillUnmount () {
+        this.clearTimeouts();
+    }
+
+    render () {
+        const { timeouts, setTimeout, clearTimeouts } = this;
+
+        return <Composition
+            timeouts={timeouts}
+            setTimeout={setTimeout}
+            clearTimeouts={clearTimeouts}
+            { ...this.props } />
+    }
+}
+
+export default Timeout;
